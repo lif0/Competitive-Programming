1e656e3d5faa776ed79256dc8692911c05104408
Kadi Gasanguseynov <kadi741@wavecom.io>
2021-09-25 13:04:17 +0300

    [Codeforces] Added 2 tasks

---
+++ codeforces-C#/Арбуз/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Арбуз
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            
+            if (w != 2 && w % 2 == 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");   
+        }
+    }
+}
---
+++ codeforces-C#/Слишком длинные слова/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Слишком_длинные_слова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (input.Length <= 10) Console.WriteLine(input);
+                else
+                {
+                    input = input.Remove(1, input.Length-2).Insert(1, input.Substring(1, input.Length - 2).Length.ToString());
+                    Console.WriteLine(input);
+                }
+            }
+        }
+    }
+}
